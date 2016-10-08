ac32aa4d8117a81e1f697c215d449d82dbfb9c9a
esolorzanob <esolorzano@renovatiocloud.com>
2016-10-08 09:43:16 -0600

    cambios juego.js

--- js/juego.js
+++ js/juego.js
@@ -1,7 +1,22 @@
 function respuesta() {
-
+	var hijos = document.getElementById("resultado").childNodes;
+	if (hijos.length > 0){
+		document.getElementById("resultado").removeChild(hijos[0]);
+	}
+	
     var selected = document.getElementById("2").checked;
-    if (selected) {
+	var contador = 0;
+	var elements = document.getElementsByClassName("radio");
+	for (var i = 0; i < elements.length; ++i) {
+        if(elements[i].checked){
+			contador++;
+		}
+    }
+	if(contador == 0){
+		 var text = document.createTextNode("Por favor escoja una respuesta");
+        document.getElementById("resultado").appendChild(text);
+        document.getElementById("resultado").classList.add("incorrecto");
+	}else if (selected) {
         var text = document.createTextNode("Correcto");
         document.getElementById("resultado").appendChild(text);
         document.getElementById("resultado").classList.add("correcto");
@@ -10,11 +25,16 @@ function respuesta() {
         document.getElementById("resultado").appendChild(text);
         document.getElementById("resultado").classList.add("incorrecto");
     }
-    var elements = document.getElementsByClassName("radio");
-    
-    for (var i = 0; i < elements.length; ++i) {
+	
+    if (contador > 0){
+		
+		for (var i = 0; i < elements.length; ++i) {
         elements[i].disabled = true;
-    }
+		}	
+		document.getElementById("button").disabled = true;	
+	}
+    
+	
 
 
 }
