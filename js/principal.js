fb2200eedebd572a395b79b105cb0c5ca6f2ac38
esolorzanob <esolorzano@renovatiocloud.com>
2016-10-14 15:14:55 -0600

    semana 6

--- js/principal.js
+++ js/principal.js
@@ -15,3 +15,25 @@ var pruebaJS = function () {
     alert("Los datos se guardaron con éxito");
 };
 
+
+var convertir = function () {
+    if(document.getElementById('masc').checked){
+        var genero = "Masculino";
+    }else if (document.getElementById('fem').checked){
+        var genero = "Femenino";
+    }else{
+        var genero = "Otro";
+    }
+    var persona = {
+        nombre: document.getElementById('nombre').value,
+        apellidos: document.getElementById('apellidos').value,
+        telefono: document.getElementById('tel').value,
+        correo: document.getElementById('correo').value,
+        direccion: document.getElementById('direccion').value,
+        genero: genero,
+        edad: document.getElementById('edad').value,
+        fecha: document.getElementById('fecha').value
+    }
+    document.getElementById('monitor').value = JSON.stringify(persona, null, 4);
+    return false;
+}
