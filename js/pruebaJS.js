3b6bff3c7a2411ea8b804ff4ca67b47d789b6424
esolorzanob <esolorzano@renovatiocloud.com>
2016-11-05 10:31:35 -0600

    local storage

--- js/pruebaJS.js
+++ js/pruebaJS.js
@@ -1,15 +1,6 @@
 
 
 
-var test = function(){  
-	var num1 = document.getElementById('num1').value;
-	var num2 = document.getElementById('num2').value;
-	var resultado = num1 * num2;
-    var todo = {
-		numero1: num1,
-		numero2: num2,
-		resultado: resultado
-	};
-	document.getElementById("monitor").value += "\n" + JSON.stringify(todo);	
+var multiJQ = function(){
+	$('#monitor').text($('#num1').val() * $('#num2').val());
 }
-
--- js/temporal.js
+++ js/temporal.js
@@ -1,26 +1,43 @@
-var convertir = function () {
+function convertir () {           
+  
+    var checked = document.getElementById('masc').checked;
+    var checked2 = document.getElementById('fem').checked;
+    
+    var genero2 = "";
     if(checked){
-        var genero = "Masculino";
-    }else if (checked){
-        var genero = "Femenino";
+        genero2 = "Masculino";
+    } else if (checked2){
+        genero2 = "Femenino";
     }else{
-        var genero = "Otro";
-    }
+        genero2 = "Otro";
+    }     
+    
     var persona = {
+               
         nombre: document.getElementById('nombre').value,
         apellidos: document.getElementById('apellidos').value,
         telefono: document.getElementById('tel').value,
         correo: document.getElementById('correo').value,
         direccion: document.getElementById('direccion').value,
-        genero: genero,
+        genero: genero2,
         edad: document.getElementById('edad').value,
-        fecha: document.getElementById('fecha').value
-    }
+        fecha: document.getElementById('fecha').value4
+       
+    };
+   
     document.getElementById('monitor').value = JSON.stringify(persona, null, 4);
-    
-    
-    
-    return false;
+    sessionStorage.setItem("persona1", JSON.stringify(persona)); 
+    return false;      
+}
+
+
+function mostrar () {
+    var persona = sessionStorage.getItem("persona1");
+     persona = JSON.parse(persona);
+     document.getElementById("nombre").value = persona.nombre;
+     document.getElementById("apellidos").value = persona.apellidos;
+     document.getElementById("tel").value = persona.telefono;
+     document.getElementById("email").value = persona.correo;
 }
 
 
