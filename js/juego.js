3612b311a82e9880ae8e1a539be27f7c67be504e
esolorzanob <esolorzano@renovatiocloud.com>
2017-03-16 15:07:50 -0600

    arreglado juego

--- js/juego.js
+++ js/juego.js
@@ -1,17 +1,20 @@
-function respuesta() {
+var respuestas = [1];
+function respuesta(number) {
 	var hijos = document.getElementById("resultado").childNodes;
 	if (hijos.length > 0){
 		document.getElementById("resultado").removeChild(hijos[0]);
 	}
-	
-    
+	    
 	var contador = 0;
 	var elements = document.getElementsByClassName("radio");
+	console.log(elements);
 	for (var i = 0; i < elements.length; ++i) {
         if(elements[i].checked){
 			contador++;
 		}
     }
+	var selected = elements[respuestas[number]].checked;
+	
 	if(contador == 0){
 		 var text = document.createTextNode("Por favor escoja una respuesta");
         document.getElementById("resultado").appendChild(text);
@@ -19,6 +22,7 @@ function respuesta() {
 	}else if (selected) {
         var text = document.createTextNode("Correcto");
         document.getElementById("resultado").appendChild(text);
+		document.getElementById("resultado").classList.remove("incorrecto");
         document.getElementById("resultado").classList.add("correcto");
     } else {
         var text = document.createTextNode("Incorrecto");
